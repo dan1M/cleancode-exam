d2a06d69cab9902aba35b68a02994e432e6a64ee
dan1M <maneadaniel555@gmail.com>
2024-06-15 19:14:12 +0200

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Flex } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Flex flexDir="column" h={'full'}>
+      ds
+    </Flex>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
