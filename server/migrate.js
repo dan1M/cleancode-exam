78464e1556bfaa6cebe20fb5a74876b0515874c2
xaless <hsuwang75@gmail.com>
2024-06-16 15:22:27 +0200

    docker

--- server/migrate.js
+++ server/migrate.js
@@ -1,42 +1,15 @@
-import db from './models/index.js';
+const db = require("./db/models");
 
-const connection = db.connection;
+const mode = process.argv[2] ?? "alter";
 
-const options = {
-  '--type': {
-    value: true,
-    default: 'alter',
-  },
-  '--force': {
-    value: false, // Par défaut, ne pas forcer la synchronisation
-  },
-  '--dir': {
-    default: 'up',
-  },
-};
-
-const args = process.argv.slice(2);
-
-do {
-  const arg = args.shift();
-  if (arg in options) {
-    if (options[arg].value) {
-      options[arg] = args.shift();
-    } else {
-      options[arg] = options[arg].default ?? true;
-    }
-  }
-} while (args.length);
-
-const syncOptions = {
-  [options['--type']]: true,
-  force: options['--force'], // Utilisez la valeur de force spécifiée dans les options
-};
-
-connection
-  .sync(syncOptions)
-  .then(() => connection.close())
-  .then(() => {
-    console.log('Database synced');
-    process.exit();
-  });
+db.connection
+    .sync({ [mode]: true })
+    .then(() =>
+        console.log(
+            "\u001b[" + 32 + "m" + "schema has been updated" + "\u001b[0m"
+        )
+    )
+    .then(() => {
+        db.connection.close();
+    })
+    .catch((err) => console.log("\u001b[1;" + 31 + "m" + err + "\u001b[0m"));
